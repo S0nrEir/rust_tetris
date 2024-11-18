1d4422d3277f97638448789fcf6645b6e54a546c
YangHaoChen <x73901@live.com>
2024-11-18 16:26:59 +0800

    引入color crate；添加打印工具

---
+++ src/tools.rs
@@ -0,0 +1,12 @@
+﻿pub mod Logger{
+    use std::fmt::Debug;
+    ///打印工具
+    /// # Arguments
+    /// * sender - 调用方 / caller
+    /// * msg - 消息 / message
+    /// # Returns
+    /// none
+    pub fn log<T:Debug>(sender:T,msg:&str) {
+        println!("{:?} {}",sender,msg);
+    }
+}
