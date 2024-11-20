dd2ab5dbf4934acaf51a3c38cfd9488230384918
YangHaoChen <x73901@live.com>
2024-11-20 18:13:53 +0800

    游戏主循环

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,45 @@ mod test;
 mod t_updatable;
 mod define;
 mod runtime;
+mod main_state;
+
+use std::{env, path};
+use crate::tools::Logger::*;
+use ggez::{
+    event,
+    glam::*,
+    graphics::{self, Color},
+    Context, GameResult,
+};
+use main_state::MainState;
+
 
 fn main() {
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } 
+    else {
+        path::PathBuf::from("./resources")
+    };
+
+    let context_builder = ggez::ContextBuilder::new("rust_tetris", "yhc")
+        .add_resource_path(resource_dir);
+    let build_result = context_builder.build();
+    let (mut context, event_loop);
+    match build_result {
+        Ok(context_builder) => {
+            context = context_builder.0;
+            event_loop = context_builder.1;
+        }
+        Err(error) => {
+            println!("{}",&error );
+            return;
+        }
+    }
     
-    // examples::example_02_enter();
+    let state = MainState::new(&mut context).unwrap();
+    event::run(context, event_loop, state);
+
 }
---
+++ src/main_state.rs
@@ -0,0 +1,40 @@
+﻿use ggez::{event,graphics::{self},Context, GameResult};
+use crate::tools::Logger::*;
+
+///游戏状态 / MainState
+pub struct MainState {
+    frames: usize,
+}
+
+impl MainState {
+    pub fn new(ctx: &mut Context) -> GameResult<MainState> {
+        ctx.gfx.add_font(
+            "consola",
+            graphics::FontData::from_path(ctx, "/consola.ttf")?,
+        );
+
+        let s = MainState { frames: 0 };
+        return Ok(s);
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        return Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(
+            ctx, 
+            graphics::Color::from([0.1, 0.2, 0.3, 1.0])
+        );
+        
+        canvas.finish(ctx)?;
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            log("MainState", &format!("FPS: {}",ctx.time.fps()), LogLevelEnum::Info);
+        }
+
+        return Ok(());
+    }
+}
