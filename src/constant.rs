5314472884a50b8c30a57c6e4731956a164410db
YangHaoChen <x73901@live.com>
2024-11-20 20:41:47 +0800

    添加全局constant

--- src/constant.rs
+++ src/constant.rs
@@ -1,1 +1,15 @@
-﻿
+﻿//--------------font----------------
+///字体名称
+pub const FONT_NAME: &str = "consola";
+///字体资产路径
+pub const FONT_ASSET_PATH: &str = "/font/consola.ttf";
+
+//--------------resource----------------
+///资源目录
+pub const RESOURCE_DIR: &str = "resource";
+
+//--------------App----------------
+///game id
+pub const APP_GAME_ID : &str = "rust_tetris";
+/// 作者 / author
+pub const APP_AUTHOR_NAME : &str = "SonrEir";
